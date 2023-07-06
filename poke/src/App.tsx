d34008908c299612242083045ed6418943ec486b
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-07-07 00:32:43 +0900

    レスポンシブ調整、◯×を表示させる

--- poke/src/App.tsx
+++ poke/src/App.tsx
@@ -11,17 +11,19 @@ function App() {
   const onClickNextQuiz = () => {
     setRandomZukan(zukanList[Math.floor(Math.random() * zukanList.length)]);
     setInputText("");
+    setResult("");
   };
   const onClickSubmitButton = () => {
     if (inputText === randomZukan.answer) {
-      console.log("正解");
+      setResult("◯");
     } else {
-      console.log("不正解");
+      setResult("×");
     }
   };
 
   const [answer, setAnswer] = useState("");
   const [inputText, setInputText] = useState("");
+  const [result, setResult] = useState("");
 
   return (
     <div className="App overflow-y-hidden">
@@ -37,10 +39,10 @@ function App() {
       </header>
       <section className="l-question relative overflow-y-scroll pb-12">
         <div className="">
-          <p className="mb-20 text-9xl font-extrabold leading-relaxed">
+          <p className="mb-16 text-7xl font-extrabold leading-relaxed md:mb-20 md:text-9xl md:leading-relaxed">
             {randomZukan.question}
           </p>
-          <p className="mb-52 text-6xl font-extrabold">
+          <p className="mb-28 text-4xl font-extrabold md:mb-52 md:text-6xl">
             『{randomZukan.game}』より
           </p>
           <p className="mb-4 pl-4 text-3xl font-bold">参考文献</p>
@@ -62,31 +64,32 @@ function App() {
           </a>
         </div>
         <p
-          className="fixed bottom-4 right-8 cursor-pointer text-xl text-blue-800"
+          className="fixed bottom-9 right-8 cursor-pointer text-xl font-bold text-blue-800"
           onClick={onClickNextQuiz}
         >
           次のクイズに進む
         </p>
       </section>
-      <section className="l-answer flex items-center border-t border-gray-900 pl-8">
-        <div className="flex items-center">
-          <div className="flex h-24 items-center border-r border-gray-900 pr-4">
+      <section className="l-answer flex items-center border-t border-gray-900 md:pl-8">
+        <div className="flex flex-wrap items-center md:flex-nowrap">
+          <div className="flex flex-wrap items-center border-r border-gray-900 pt-4 md:h-24 md:flex-nowrap md:pr-4 md:pt-0">
             <input
               type="text"
               placeholder="ポケモンの名前"
-              className="h-16 w-72 cursor-pointer p-2 text-2xl"
+              className="w-72 cursor-pointer p-2 text-2xl md:h-16"
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
             />
             <p className="mx-6 text-4xl font-bold">に</p>
             <button
-              className="h-16 w-72 rounded-full bg-red-700 text-3xl font-bold text-white"
+              className="mt-4 h-16 w-72 rounded-full bg-red-700 text-3xl font-bold text-white md:mt-0"
               onClick={onClickSubmitButton}
             >
               きめた！
             </button>
           </div>
           <div className="flex flex-wrap items-center pl-8">
+            <p className="mr-16 w-4 text-6xl font-bold">{result}</p>
             <p className="mr-2 block text-2xl font-bold">こたえは</p>
             <p className="text-4xl font-bold text-red-700">
               {randomZukan.answer}
